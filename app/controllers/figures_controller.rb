d67422afcdcba197ab4a1550b5a686c2f48b9c9b
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-11-02 12:49:21 -0400

    completed new figure route

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,18 +2,33 @@ class FiguresController < ApplicationController
 
   get '/figures' do
     # binding.pry
+    @figures = Figure.all
     erb :'./figures/index'
   end
 
   get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    # binding.pry
     erb :'/figures/new'
   end
 
   post '/figures/new' do
+    # binding.pry
+    @figure = Figure.create(params["figure"])
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params["landmark"])
+    end
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params["title"])
+    end
 
+    binding.pry
   end
 
   get '/figures/:id' do
 
   end
+
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
-Bundler.require
 
+Bundler.require
 ENV['SINATRA_ENV'] ||= "development"
 
 ActiveRecord::Base.establish_connection(
